aef445cbcf16a6b753d0fa38482a569f562da6fe
高岡祐里 <tak@takaokayurinoiMac.local>
2019-08-06 15:08:36 +0900

    色々変更した

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Blog.create([{ title: '記事タイトル1', content: '記事の内容1' }])
+Blog.create([{ title: '記事タイトル2', content: '記事の内容2' }])
+Blog.create([{ title: '記事タイトル3', content: '記事の内容3' }])
+Blog.create([{ title: '記事タイトル4', content: '記事の内容4' }])
+Blog.create([{ title: '記事タイトル5', content: '記事の内容5' }])
+Blog.create([{ title: '記事タイトル6', content: '記事の内容6' }])
+Blog.create([{ title: '記事タイトル7', content: '記事の内容7' }])
+Blog.create([{ title: '記事タイトル8', content: '記事の内容8' }])
+Blog.create([{ title: '記事タイトル9', content: '記事の内容9' }])
+Blog.create([{ title: '記事タイトル10', content: '記事の内容10' }])
