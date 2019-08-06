071f70693cb2c4bc44972e0aa8433af6f8da8c2d
高岡祐里 <tak@takaokayurinoiMac.local>
2019-08-06 16:44:13 +0900

    helloworld

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,23 @@
+class BlogsController < ApplicationController
+  def index
+    render text: 'Hello, World!!'
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  #get 'blogs/index'
+  #get 'blogs/new'
+  #get 'blogs/create'
+  #get 'blogs/show'
+  #get 'blogs/edit'
+  #get 'blogs/update'
+  #get 'blogs/destroy'
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
