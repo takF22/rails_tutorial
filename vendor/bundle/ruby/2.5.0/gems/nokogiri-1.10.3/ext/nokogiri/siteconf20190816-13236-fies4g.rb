4dcd779834fb47be93b056c67eba3720159ababb
高岡祐里 <tak@takaokayurinoiMac.local>
2019-08-16 14:46:32 +0900

    DLした

--- vendor/bundle/ruby/2.5.0/gems/nokogiri-1.10.3/ext/nokogiri/siteconf20190816-13236-fies4g.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20190816-13236-1mwbgkw"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
